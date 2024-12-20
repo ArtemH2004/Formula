4bdcc317571cf41fddb2ad1db27cf601e9fb1d30
ArtemH2004 <159913643+ArtemH2004@users.noreply.github.com>
2024-12-20 22:27:49 +0300

    models

---
+++ Models/Race.cs
@@ -0,0 +1,16 @@
+﻿namespace Formula.Models
+{
+    public class Race
+    {
+        public int RaceId { get; set; }
+        public DateTime Date { get; set; }
+        public int StageNumber { get; set; }
+        public string Type { get; set; }
+        public int Price { get; set; }
+        public int AudienceCount { get; set; }
+        public string Result { get; set; } // ???   mb name of win team 
+        public string Weather { get; set; }
+        public int TrackId { get; set; }
+        public Track Track { get; set; }
+    }
+}
---
+++ Models/Staff.cs
@@ -0,0 +1,13 @@
+﻿namespace Formula.Models
+{
+    public class Staff
+    {
+        public int StaffId { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime Birthday { get; set; }
+        public char Gender { get; set; }
+        public string Job { get; set; }
+        public int TeamId { get; set; }
+    }
+}
